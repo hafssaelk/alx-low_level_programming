7a8dc13116011bdea4e5d65e2b2af99097d0672e
hafsa elkoi <hafssa.elkoi2@gmail.com>
2023-08-15 21:22:28 +0100

    Merged remote changes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#include "main.h"
-#include <unistd.h>
 /**
  * * _putchar - writes the character c to stdout
  * * @c: The character to print
@@ -7,11 +5,7 @@
  *   * Return: On success 1.
  *    * On error, -1 is returned, and errno is set appropriately.
  *     */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-main.h CODE
+
 #ifndef main_h
 #define main_h
 
