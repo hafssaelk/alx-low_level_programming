fc3f7c29bae83d5c597b9798d657dd4ce7071546
hafssaelk <138696982+hafssaelk@users.noreply.github.com>
2023-08-21 19:44:42 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,9 @@
-#include "main.h"
+# include "main.h"
 
 /**
- * _strlen - returns length of string
- * @s: string
- * Return: length of the string, excluding the null-terminator
+ * _strlen - returns length of string .
+ * @s: string .
+ * Return : length of the string, excluding the null-terminator .
  */
 
 int _strlen(char *s)
