e28d374011dedc6556426b2d641d5a7314698397
hafssaelk <138696982+hafssaelk@users.noreply.github.com>
2023-09-07 13:53:12 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,52 +1,29 @@
 #include "main.h"
 
-
 /**
  * main - multiplies two positive numbers
- * @argc: number of arguments
- * @argv: array of argument strings
- * Return: 0 on success, 98 on error
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
  */
 int main(int argc, char *argv[])
 {
-    long num1, num2;
-    long mul;
-    int i, j;
+unsigned long mul;
+int i, j;
+	if (argc != 3)
+	{ printf("Error\n");
+	exit(98); }
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{  printf("Error\n");
+			exit(98); }
+		}
 
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return (98);
-    }
-
-    for (i = 1; i < argc; i++)
-    {
-        for (j = 0; argv[i][j] != '\0'; j++)
-        {
-            if (j == 0 && (argv[i][j] == '+' || argv[i][j] == '-'))
-            {
-                // Allow leading plus or minus sign
-                continue;
-            }
-            if (!isdigit(argv[i][j]))
-            {
-                printf("Error\n");
-                return (98);
-            }
-        }
-    }
-
-    num1 = atol(argv[1]);
-    num2 = atol(argv[2]);
-
-    if (num1 <= 0 || num2 <= 0)
-    {
-        printf("Error\n");
-        return (98);
-    }
-
-    mul = num1 * num2;
-    printf("%ld\n", mul);
-
-    return (0);
+	}
+	mul = atol(argv[1]) *  atol(argv[2]);
+	printf("%lu\n", mul);
+return (0);
 }
